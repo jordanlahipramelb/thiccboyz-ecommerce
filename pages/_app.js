14b370961a7aa7c01f188b9520630c1779cab974
Jordan Lahip-Ramelb <jordan.lahipramelb@gmail.com>
2022-06-21 10:30:10 -0700

    added stripe payment function

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/globals.css";
 import { Layout } from "../components";
-// pass context to components inside of it
+// pass state/context to components inside of it
 import { StateContext } from "../context/StateContext";
 // small notification popup
 import { Toaster } from "react-hot-toast";
